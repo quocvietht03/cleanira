311256d6f7b9af1d7bd2a650fba49c2294289305
Duc Dung <ducdung@Dung-Mac-mini.local>
2024-11-12 15:51:53 +0700

    section service, section choose us

--- framework/widgets/service-loop-item/widget.php
+++ framework/widgets/service-loop-item/widget.php
@@ -28,54 +28,11 @@ class Widget_ServiceLoopItem extends Widget_Base {
 		return [ 'cleanira' ];
 	}
 
-	protected function register_layout_section_controls() {
-		$this->start_controls_section(
-			'section_content',
-			[
-				'label' => __( 'Content', 'cleanira' ),
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Image_Size::get_type(),
-			[
-				'name' => 'thumbnail',
-				'label' => __( 'Image Size', 'cleanira' ),
-				'show_label' => true,
-				'default' => 'medium',
-				'exclude' => [ 'custom' ],
-			]
-		);
-
-		$this->add_responsive_control(
-			'image_ratio',[
-				'label' => __( 'Image Ratio', 'cleanira' ),
-				'type' => Controls_Manager::SLIDER,
-				'default' => [
-					'size' => 0.64,
-				],
-				'range' => [
-					'px' => [
-						'min' => 0.3,
-						'max' => 2,
-						'step' => 0.01,
-					],
-				],
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--featured .bt-cover-image' => 'padding-bottom: calc( {{SIZE}} * 100% );',
-				],
-			]
-		);
-
-		$this->end_controls_section();
-	}
-
 	protected function register_style_section_controls() {
-
 		$this->start_controls_section(
-			'section_style_image',
+			'section_style_box',
 			[
-				'label' => esc_html__( 'Image', 'cleanira' ),
+				'label' => esc_html__( 'Box', 'cleanira' ),
 				'tab' => Controls_Manager::TAB_STYLE,
 			]
 		);
@@ -87,11 +44,36 @@ class Widget_ServiceLoopItem extends Widget_Base {
 				'type' => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%' ],
 				'selectors' => [
-					'{{WRAPPER}} .bt-post--featured .bt-cover-image' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'{{WRAPPER}} .bt-post--inner' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
-
+		$this->add_control(
+			'background_box',[
+				'label'     => esc_html__( 'Background Box', 'cleanira' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}} .bt-post--inner' => 'background: {{VALUE}};',
+				],
+			]
+		);
+		$this->add_control(
+			'image_style',[
+				'label' => esc_html__( 'Image', 'cleanira' ),
+				'type'  => Controls_Manager::HEADING,
+			]
+		);
+		$this->add_control(
+			'overlay_image',[
+				'label'     => esc_html__( 'Overlay Image Color', 'cleanira' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}} .bt-post--image svg path' => 'fill: {{VALUE}};',
+				],
+			]
+		);
 		$this->start_controls_tabs( 'thumbnail_effects_tabs' );
 
 		$this->start_controls_tab( 'thumbnail_tab_normal',
@@ -103,7 +85,7 @@ class Widget_ServiceLoopItem extends Widget_Base {
 		$this->add_group_control(
 			Group_Control_Css_Filter::get_type(),[
 				'name' => 'thumbnail_filters',
-				'selector' => '{{WRAPPER}} .bt-post--featured img',
+				'selector' => '{{WRAPPER}} .bt-post--image img',
 			]
 		);
 
@@ -117,7 +99,7 @@ class Widget_ServiceLoopItem extends Widget_Base {
 		$this->add_group_control(
 			Group_Control_Css_Filter::get_type(),[
 				'name'     => 'thumbnail_hover_filters',
-				'selector' => '{{WRAPPER}} .bt-post:hover .bt-post--featured img',
+				'selector' => '{{WRAPPER}} .bt-post:hover .bt-post--image img',
 			]
 		);
 
@@ -133,16 +115,7 @@ class Widget_ServiceLoopItem extends Widget_Base {
 				'tab' => Controls_Manager::TAB_STYLE,
 			]
 		);
-		$this->add_control(
-			'background_content',[
-				'label'     => esc_html__( 'Background Content', 'cleanira' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--content' => 'background: {{VALUE}};',
-				],
-			]
-		);
+		
 		$this->add_control(
 			'title_style',[
 				'label' => esc_html__( 'Title', 'cleanira' ),
@@ -182,158 +155,47 @@ class Widget_ServiceLoopItem extends Widget_Base {
 		);
 
 		$this->add_control(
-			'description_style',[
-				'label' => esc_html__( 'Description', 'cleanira' ),
+			'service_types_style',[
+				'label' => esc_html__( 'Service Types', 'cleanira' ),
 				'type'  => Controls_Manager::HEADING,
 			]
 		);
 		$this->add_control(
-			'description_color',[
+			'service_types_icon_color',[
+				'label'     => esc_html__( 'Icon Color', 'cleanira' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}} .bt-type-item svg path' => 'fill: {{VALUE}};',
+				],
+			]
+		);
+		$this->add_control(
+			'service_types_color',[
 				'label'     => esc_html__( 'Color', 'cleanira' ),
 				'type'      => Controls_Manager::COLOR,
 				'default'   => '',
 				'selectors' => [
-					'{{WRAPPER}} .bt-post--description' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .bt-type-item' => 'color: {{VALUE}};',
 				],
 			]
 		);
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
 			[
-				'name'     => 'description_typography',
+				'name'     => 'service_types_typography',
 				'label'    => esc_html__( 'Typography', 'cleanira' ),
 				'default'  => '',
-				'selector' => '{{WRAPPER}} .bt-post--description',
-			]
-		);
-		$this->add_control(
-			'price_style',[
-				'label' => esc_html__( 'Price', 'cleanira' ),
-				'type'  => Controls_Manager::HEADING,
+				'selector' => '{{WRAPPER}} .bt-type-item',
 			]
 		);
 
-		$this->add_control(
-			'price_color',[
-				'label'     => esc_html__( 'Color', 'cleanira' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--price' => 'color: {{VALUE}};',
-				],
-			]
-		);
-		$this->add_control(
-			'price_background',[
-				'label'     => esc_html__( 'Background', 'cleanira' ),
-				'type'      => Controls_Manager::COLOR,
-				'default'   => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--price' => 'background: {{VALUE}};',
-					'{{WRAPPER}} .bt-post--price:before' => 'border-top-color: {{VALUE}};',
-					'{{WRAPPER}} .bt-post--price:after' => 'border-bottom-color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
-				'name'     => 'price_typography',
-				'label'    => esc_html__( 'Typography', 'cleanira' ),
-				'default'  => '',
-				'selector' => '{{WRAPPER}} .bt-post--price',
-			]
-		);
-		$this->add_control(
-			'button_style',[
-				'label' => esc_html__( 'Button', 'cleanira' ),
-				'type'  => Controls_Manager::HEADING,
-			]
-		);
-		$this->start_controls_tabs( 'button_style_tabs' );
-
-		$this->start_controls_tab( 'style_normal',
-			[
-				'label' => __( 'Normal', 'cleanira' ),
-			]
-		);
-
-		$this->add_control(
-			'button_text_color',
-			[
-				'label' => __( 'Text Color', 'cleanira' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--button-booknow a' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'button_bg_color',
-			[
-				'label' => __( 'Background Color', 'cleanira' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--button-booknow a' => 'background-color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->start_controls_tab( 'style_hover',
-			[
-				'label' => __( 'Hover', 'cleanira' ),
-			]
-		);
-
-		$this->add_control(
-			'button_text_color_hover',
-			[
-				'label' => __( 'Text Color', 'cleanira' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--button-booknow a:hover' => 'color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->add_control(
-			'button_bg_color_hover',
-			[
-				'label' => __( 'Background Color', 'cleanira' ),
-				'type' => Controls_Manager::COLOR,
-				'default' => '',
-				'selectors' => [
-					'{{WRAPPER}} .bt-post--button-booknow a:hover' => 'background-color: {{VALUE}}; border-color: {{VALUE}};',
-				],
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->end_controls_tabs();
-		$this->add_group_control(
-			Group_Control_Typography::get_type(),
-			[
-				'name'     => 'button_typography',
-				'label'    => esc_html__( 'Typography', 'cleanira' ),
-				'default'  => '',
-				'selector' => '{{WRAPPER}} .bt-post--button-booknow a',
-			]
-		);
 
 		$this->end_controls_section();
 
 	}
 
 	protected function register_controls() {
-		$this->register_layout_section_controls();
 		$this->register_style_section_controls();
 	}
 
