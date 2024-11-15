70f69dcb8bbf8d878e1e509edb739d74d7f7cb12
Duc Dung <ducdung@Dung-Mac-mini.local>
2024-11-15 15:28:30 +0700

    icon list service

--- framework/widgets/services-list/widget.php
+++ framework/widgets/services-list/widget.php
@@ -467,7 +467,9 @@ class Widget_ServicesList extends Widget_Base
 								<?php if (!empty($service_types)): ?>
 									<ul class="bt-service-types">
 										<?php foreach ($service_types as $type): ?>
-											<li class="bt-type-item"><?php echo esc_html($type['name']) ?></li>
+											<li class="bt-type-item"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+												<path fill-rule="evenodd" clip-rule="evenodd" d="M21.0573 7.00193C21.4381 7.40195 21.4225 8.03492 21.0225 8.41571L9.46695 19.4157C9.26961 19.6036 9.00417 19.7028 8.732 19.6904C8.45984 19.678 8.2045 19.5551 8.02505 19.3501L3.5806 14.2732C3.21682 13.8576 3.25879 13.2258 3.67434 12.8621C4.08989 12.4983 4.72166 12.5403 5.08544 12.9558L8.84314 17.2483L19.6435 6.9671C20.0436 6.58631 20.6765 6.60191 21.0573 7.00193Z" fill="#2D77DC" />
+											</svg><?php echo esc_html($type['name']) ?></li>
 										<?php endforeach; ?>
 									</ul>
 								<?php endif; ?>
