54453a2cd8f3a77b3b905b39696d2436b0c37471
Duc Dung <ducdung@Dung-Mac-mini.local>
2024-11-19 08:56:24 +0700

    page blog

--- index.php
+++ index.php
@@ -1,36 +1,27 @@
 <?php
 get_header();
-get_template_part( 'framework/templates/site', 'titlebar');
+get_template_part('framework/templates/site', 'titlebar');
 
 ?>
 <main id="bt_main" class="bt-site-main">
 	<div class="bt-main-content-ss">
 		<div class="bt-container">
-			<div class="bt-main-post-row">
-				<div class="bt-main-post-col">
+			<?php
+			if (have_posts()) {
+			?>
+				<div class="bt-list-post">
 					<?php
-						if( have_posts() ) {
-							?>
-								<div class="bt-list-post">
-									<?php
-										while ( have_posts() ) : the_post();
-											get_template_part( 'framework/templates/post');
-										endwhile;
-									?>
-								</div>
-							<?php
-							cleanira_paging_nav();
-						} else {
-							get_template_part( 'framework/templates/post', 'none');
-						}
+					while (have_posts()) : the_post();
+						get_template_part('framework/templates/post', 'style', array('image-size' => 'large'));
+					endwhile;
 					?>
 				</div>
-				<div class="bt-sidebar-col">
-					<div class="bt-sidebar">
-						<?php if(is_active_sidebar('main-sidebar')) echo get_sidebar('main-sidebar'); ?>
-					</div>
-				</div>
-			</div>
+			<?php
+				cleanira_paging_nav();
+			} else {
+				get_template_part('framework/templates/post', 'none');
+			}
+			?>
 		</div>
 	</div>
 
