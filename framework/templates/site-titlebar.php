ba7ed9d9b52614fbd7c48cd0e471aa0916f0b8c9
Duc Dung <ducdung@Dung-Mac-mini.local>
2024-12-14 10:10:16 +0700

    update options titlebar

--- framework/templates/site-titlebar.php
+++ framework/templates/site-titlebar.php
@@ -5,20 +5,45 @@
  *  
  */
 if (function_exists('get_field')) {
-  $background_color = !empty(get_field('title_bar_bg_color', 'options')) ? get_field('title_bar_bg_color', 'options') : '';
-}else{
+  $background_color = get_field('title_bar_bg_color', 'options') ?: '';
+  $background_image = get_field('title_bar_bg_images', 'options') ?: '';
+  $bubble_enable = get_field('bubble_enable', 'options');
+  $wave_enable = get_field('wave_enable', 'options');
+  $title_bar_bg_overlay = get_field('title_bar_bg_overlay', 'options') ?: '';
+} else {
   $background_color = '';
+  $background_image = '';
+  $title_bar_bg_overlay = '';
+  $bubble_enable = true;
+  $wave_enable = true;
+}
+$style_attributes = '';
+if ($background_color || $background_image) {
+  $style_parts = [];
+  if ($background_color) {
+    $style_parts[] = 'background-color: ' . esc_attr($background_color) . ';';
+  }
+  if ($background_image) {
+    $style_parts[] = 'background-image: url(' . esc_url($background_image['url']) . ');';
+  }
+  $style_attributes = implode(' ', $style_parts);
 }
 ?>
-<section class="bt-site-titlebar" <?php echo !empty($background_color) ? ' style="background-color: ' . esc_attr($background_color) . ';"' : ''; ?>>
+
+<section class="bt-site-titlebar<?php echo esc_attr($bubble_enable ? ' bt-has-bubble' : ' bt-no-bubble'); ?>" <?php echo 'style="' . $style_attributes . '"'; ?>>
+  <?php if ($title_bar_bg_overlay) {
+    echo '<div class="bt-titlebar-overlay" style="background-color:' . esc_attr($title_bar_bg_overlay) . '"></div>';
+  } ?>
   <div class="bt-container">
-    <div class="bt-wave bt-wave-bottom" data-negative="false">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
-        <path class="bt-wave-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
-	c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
-	c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
-      </svg>
-    </div>
+    <?php if ($wave_enable): ?>
+      <div class="bt-wave bt-wave-bottom" data-negative="false">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+          <path class="bt-wave-fill" d="M421.9,6.5c22.6-2.5,51.5,0.4,75.5,5.3c23.6,4.9,70.9,23.5,100.5,35.7c75.8,32.2,133.7,44.5,192.6,49.7
+                  c23.6,2.1,48.7,3.5,103.4-2.5c54.7-6,106.2-25.6,106.2-25.6V0H0v30.3c0,0,72,32.6,158.4,30.5c39.2-0.7,92.8-6.7,134-22.4
+                  c21.2-8.1,52.2-18.2,79.7-24.2C399.3,7.9,411.6,7.5,421.9,6.5z"></path>
+        </svg>
+      </div>
+    <?php endif; ?>
     <div class="bt-page-titlebar">
       <div class="bt-page-titlebar--breadcrumb">
         <?php
