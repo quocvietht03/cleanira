6f3e6f944b5051b128a688f43e986b5ded96a360
tomdevvn90 <tomdevvn90@gmail.com>
2025-06-13 15:48:39 +0700

    update security import function

--- install/import-pack/ajax.php
+++ install/import-pack/ajax.php
@@ -39,37 +39,22 @@ if( ! function_exists( 'cleanira_import_pack_import_action_ajax_callback' ) ) {
 
         extract( $_POST );
 
-        if( ! isset( $data['form_data'] ) ) {
+        if( ! isset( $data['form_data'] ) || ! isset( $data['form_data'][$data['action_type']] ) || ! function_exists( $data['form_data'][$data['action_type']] ) || $data['form_data'][$data['action_type']] !== 'cleanira_import_pack_backup_site_skip_func' ) {
             wp_send_json( [
                 'success' => true,
                 'type' => 'error',
-                'message' => __( 'Error: Form data not defined!', 'cleanira' ),
+                'message' => __( 'Demo import failed. Please open a ticket for support!', 'cleanira' ),
             ] );
-        }
-
-        if( ! isset( $data['form_data'][$data['action_type']] ) ) {
+        } else {
+            $result = call_user_func( $data['form_data'][$data['action_type']] );
             wp_send_json( [
                 'success' => true,
-                'type' => 'error',
-                'message' => __( 'Error: Missing function ajax callback!', 'cleanira' ),
+                'type' => 'success',
+                'result' => $result,
             ] );
         }
 
-        if( ! function_exists( $data['form_data'][$data['action_type']] ) ) {
-            wp_send_json( [
-                'success' => true,
-                'type' => 'error',
-                'message' => __( 'Error: Function ajax not defined!', 'cleanira' ),
-            ] );
-        }
-
-        $result = call_user_func( $data['form_data'][$data['action_type']] );
-
-        wp_send_json( [
-            'success' => true,
-            'type' => 'success',
-            'result' => $result,
-        ] ); exit();
+        exit();
     }
 
     add_action( 'wp_ajax_cleanira_import_pack_import_action_ajax_callback', 'cleanira_import_pack_import_action_ajax_callback' );
