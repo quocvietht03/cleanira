6066c9fef163efbb9086193f695baed9c31c1e1b
tomdevvn90 <tomdevvn90@gmail.com>
2025-10-02 09:01:59 +0700

    create mobile menu widget

--- framework/widget-load.php
+++ framework/widget-load.php
@@ -53,6 +53,7 @@ class ElementorWidgets
 			'site-copyright',
 			'instagram-posts',
 			'page-breadcrumb',
+			'mobile-menu',
 			'post-grid',
 			'post-grid-style-1',
 			'post-loop-item',
@@ -168,6 +169,7 @@ class ElementorWidgets
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\SiteCopyright\Widget_SiteCopyright());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\InstagramPosts\Widget_InstagramPosts());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PageBreadcrumb\Widget_PageBreadcrumb());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\MobileMenu\Widget_MobileMenu());
 
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostGrid\Widget_PostGrid());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostGridStyle1\Widget_PostGridStyle1());
